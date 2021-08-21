1af59314a7cacffb16b92f79baad3129eaf08fb8
vinod096 <vinod.raipati@hotmail.com>
2021-08-21 13:13:21 +1000

    letters

---
+++ letter.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# create numpy array of letters a-j
+letter_array = np.array([['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']])
+print("Letter Array: ", letter_array)
+
+# get dtype of array
+
+#print('dtype of array : ', letter_array.dtype())
+
+# get shape of array
+
+print('shape of array : ', letter_array.shape)
+
+# get size of array
+
+print('Size of array : ', letter_array.size, 'elements')
