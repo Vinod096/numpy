7a2ac81541d0d1f48f83c05d363fa43707e86a3d
vinod096 <vinod.raipati@hotmail.com>
2021-08-28 11:41:37 +1000

    array_broadcasting

---
+++ array_broadcasting.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+# Use Broadcasting to create a 4 x 4 ndarray that has its first
+# column full of 1s, its second column full of 2s, its third
+# column full of 3s, etc..
+
+# Do not change the name of this array.
+# Please don't print anything from your code! The TEST RUN button below will print your array.
+#X = np.ones((4, 4)) * np.arange(1, 5)
+
+x= np.ones((4,4))
+print("x :",x)
+
+y = np.arange(1,5)
+print("Y : ", y)
+
+z = x * y
+print( "Z : ",z)
